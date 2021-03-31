77915901746f15f8f633c8caa29095fa0cf048c0
andreii1907 <luchianandrei1024@yahoo.com>
2021-03-31 17:25:39 +0300

    Array practice 1

--- Array/script.js
+++ Array/script.js
@@ -1,8 +1,12 @@
+// ------- Typeof Array ------- //
+
 const multipleData = [1, 'string', false, null, undefined];
 
 for (var i = 0; i < multipleData.length; i++){
     console.log(typeof multipleData[i]);
 }
+
+// ------- Cel mai mic numar ------ //
 
 const numbers = [45, 16, 19, 24, -46, 52];
 let minim = numbers[0];
@@ -14,3 +18,14 @@ for (j = 1; j < numbers.length; j++){
 }
 
 console.log('Cel mai mic numar este ' + minim);
+
+// ------- Cel mai mare numar ------- //
+
+let maxim = numbers[0];
+for (j = 1; j < numbers.length; j++){
+    if(numbers[j] > minim){
+    maxim = numbers[j];
+    }
+}
+
+console.log('Cel mai mare numar este ' + maxim);
